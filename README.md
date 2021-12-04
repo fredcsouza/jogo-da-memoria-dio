f63b3459b4086b9fb11058cf54d2565f8e8b23cd
Frederico <fredcsouza@gmail.com>
2021-12-03 23:47:59 -0300

    alteraçao do layout

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Jogo da memória
 
-Desafio do bootcamp da Digital Innovation One, criar jogo da memória utilizando HTM, CSS e Javascript.
+ Desafio do bootcamp da Digital Innovation One, criar um jogo da memória.
+
+## Tecnologias usadas:
+ - HTML 5
+ - CSS 3
+ - Javascript
+
+## Instalação
+1. Baixe o arquivo no link [jogo da memória](https://github.com/fredcsouza/jogo-da-memoria-dio/archive/refs/heads/master.zip).
+2. Descompacte e execute o arquivo index.html.
+
+## preview
+</br>
+
+<img src="./preview1.jpg" alt="preview inicio do jogo" width=500>
+<img src="./preview2.jpg" alt="preview de cartas viradas" width=500>
+<img src="./preview3.jpg" alt="preview de fim do jogo" width=500>
--- script.js
+++ script.js
@@ -1,43 +1,41 @@
 const cards = document.querySelectorAll('.card');
+const jogadasScore = document.querySelector('.jogadas')
+const errosScore = document.querySelector('.erros')
+const btnReiniciar = document.querySelector('.btn-reiniciar')
+const modal = document.querySelector('.wrapper-modal')
 
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
+let jogadas = 0;
+let acertos = 0;
+let erros = 0;
 
+modal.style.display = 'none';
 
-function flipCard() {
-    if (lockBoard) return;
-    if (this === firstCard) return;
+const setModal = (display, delay) => {
+    setTimeout(() => modal.style.display = display, delay)
+}
+const setMatch = (card, value) => card.dataset.match = value;
 
-    this.classList.add('flip');
-    if (!hasFlippedCard) {
-        hasFlippedCard = true;
-        firstCard = this;
-        return;
-    }
+const setFlip = (card, value) => value ? card.classList.add('flip') : card.classList.remove('flip');
 
-    secondCard = this;
-    hasFlippedCard = false;
-    checkForMatch();
+const shuffle = () => {
+    cards.forEach(card => (card.style.order = Math.floor(Math.random() * 12)))
+};
+
+const resetBoard = () => {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
-function checkForMatch() {
-    if (firstCard.dataset.card === secondCard.dataset.card) {
-        disableCards();
-        return;
-    }
-
-    unflipCards();
-}
-
-function disableCards() {
-    firstCard.removeEventListener('click', flipCard);
-    secondCard.removeEventListener('click', flipCard);
-
+const disableCards = () => {
+    setMatch(firstCard, true);
+    setMatch(secondCard, true);
     resetBoard();
 }
 
-function unflipCards() {
+const unflipCards = () => {
     lockBoard = true;
 
     setTimeout(() => {
@@ -48,18 +46,55 @@ function unflipCards() {
     }, 1500);
 }
 
-function resetBoard() {
-    [hasFlippedCard, lockBoard] = [false, false];
-    [firstCard, secondCard] = [null, null];
+const checkForMatch = () => {
+    if (firstCard.dataset.card === secondCard.dataset.card) {
+        acertos++;
+        disableCards();
+        (acertos === 6) && winner();
+        return;
+    }
+    erros++;
+    unflipCards();
 }
 
-(function shuffle() {
-    cards.forEach((card) => {
-        let ramdomPosition = Math.floor(Math.random() * 12);
-        card.style.order = ramdomPosition;
-    })
-})();
+const winner = () => {
+    setModal('flex', 1200)
+    jogadasScore.innerText = `Jogadas: ${jogadas}`;
+    errosScore.innerText = `Erros: ${erros}`;
+}
 
-cards.forEach((card) => {
-    card.addEventListener('click', flipCard)
-});
+const resetGame = () => {
+    setModal('none', 0)
+    cards.forEach((card) => {
+        card.classList.remove('flip')
+        card.dataset.match = false
+    });
+    jogadas = 0;
+    acertos = 0;
+    erros = 0;
+    resetBoard();
+    shuffle();
+}
+
+function flipCard() {
+    if ((lockBoard) ||
+        (this.dataset.match === 'true') ||
+        (this === firstCard)
+    ) return;
+
+    this.classList.add('flip');
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    jogadas++;
+    checkForMatch();
+}
+
+cards.forEach((card) => card.addEventListener('click', flipCard));
+btnReiniciar.addEventListener('click', resetGame);
+shuffle();
